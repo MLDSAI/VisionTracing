10c10292ccfc3c41bf931366b809f30cdd00b866
Richard Abrich <richard.abrich@gmail.com>
2020-07-21 03:28:25 -0400

    add missing

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from rq import Queue
 
@@ -6,6 +8,21 @@ from utils import count_words_at_url
 
 
 app = Flask(__name__)
+# TODO: read from environment
+# XXX DEBUG -> INFO
+app.config['LOG_LEVEL'] = os.getenv('LOG_LEVEL', 'DEBUG')
+# TODO: get rid of this
+app.config['TIMEZONE'] = os.getenv('TIMEZONE', 'America/Toronto')
+
+from logger_setup import logger
+
+logger.warning('*' * 40)
+DEBUG = os.environ.get('DEBUG', 0)
+logger.warning('DEBUG: {}'.format(DEBUG))
+is_debug = bool(int(DEBUG))
+logger.warning('is_debug: {}'.format(is_debug))
+app.debug = is_debug
+logger.warning('*' * 40)
 
 
 @app.route('/')
