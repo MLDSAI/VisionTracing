33e3c4579ca0a6f275174eb31d8cf4612c5dc92c
Richard Abrich <richard.abrich@gmail.com>
2020-07-21 02:39:00 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from rq import Queue
+
+from worker import conn
+from utils import count_words_at_url
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+  q = Queue(connection=conn)
+  result = q.enqueue(count_words_at_url, 'http://heroku.com')
+  return 'Enqueued'
