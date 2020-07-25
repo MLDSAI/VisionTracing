7084ce15937380062e84936543acc00d68fe058f
Richard Abrich <richard.abrich@gmail.com>
2020-07-25 02:13:46 -0400

    log rval

--- worker/utils.py
+++ worker/utils.py
@@ -1,6 +1,10 @@
 import requests
 
+from worker import log
+
 
 def count_words_at_url(url):
   resp = requests.get(url)
-  return len(resp.text.split())
+  rval = len(resp.text.split())
+  log('rval:', rval)
+  return rval
