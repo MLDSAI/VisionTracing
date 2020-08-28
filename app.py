cbdad6997bd6f09e50f2bdcd70fe0babd58db6f9
Andrew <andrew@admins-MacBook-Air-2.local>
2020-08-28 16:14:24 -0400

    Env and app

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ S3_SECRET_ACCESS_KEY = os.getenv('S3_SECRET_ACCESS_KEY')
 
 s3 = boto3.client(
         's3',
-        aws_access_key=S3_KEY,
+        aws_access_key_id=S3_KEY,
         aws_secret_access_key=S3_SECRET_ACCESS_KEY
         )
 
