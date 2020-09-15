d58445a475d4f8711cf4afb38eeb7a822cac7585
DrJessop <ubuntu@ip-172-31-69-86.ec2.internal>
2020-09-15 18:22:13 +0000

    Remove spurious whitespaces

--- app.py
+++ app.py
@@ -30,18 +30,18 @@ socket = SocketIO(
             cors_allowed_origins='*',
             message_queue=os.getenv('REDIS_URL'),
             manage_session=False
-         ) 
+         )
 
 @app.template_filter('empty_session')
 def empty_session(session):
     '''
-    This function removes all killed/timed-out jobs from the session if the video path for a 
-    given job does not exist. Afterwards, it specifies whether or not the session object is 
+    This function removes all killed/timed-out jobs from the session if the video path for a
+    given job does not exist. Afterwards, it specifies whether or not the session object is
     empty.
     Parameters:
     - session: Flask session object
     '''
-   
+
     if 'jobs' not in session:
         logger.info('Empty session')
         return True
@@ -50,22 +50,22 @@ def empty_session(session):
         for idx, job_dict in enumerate(session['jobs']):
             job = q.fetch_job(job_dict['id'])
             video_source = 'static/videos/{}'.format(job_dict['tracks_filename'])
-            
+
             dead_job_or_timeout = job is None or job.get_status() == 'failed'
             video_does_not_exist = not os.path.exists(video_source)
-            
+
             if dead_job_or_timeout and video_does_not_exist:
                 session['jobs'].pop(idx)
                 # Have to set modified to True after every in-place operation
-                session.modified = True 
+                session.modified = True
                 logger.info('Popped job from session: id={}'.format(job_dict['id']))
 
     clear_session()
-    
+
     if len(session['jobs']) == 0:
         logger.info('Empty session')
         return True
-    
+
     return False
 
 @app.template_filter('refresh_job')
@@ -82,14 +82,14 @@ def refresh_job(job_dict):
     try:
         job.refresh()
         logger.info('Job refreshed successfully')
-    except Exception as e: 
+    except Exception as e:
         logger.info('Job did not refresh properly, exception {}'.format(e))
     return filename
 
 @app.template_filter('display_job')
 def display_job(job_dict):
     '''
-    This function checks whether or not a video with the job's track filename 
+    This function checks whether or not a video with the job's track filename
     exists. If it exists, then it returns HTML markup for the video source.
     Else, it returns the progress information.
     Parameters:
@@ -104,7 +104,7 @@ def display_job(job_dict):
         </video>
         """.format(video_source))
     job = q.fetch_job(job_dict['id'])
-    if job and job.meta.get('status'): 
+    if job and job.meta.get('status'):
         return job.meta.get('status')
     return 'Beginning process...'
 
@@ -120,11 +120,11 @@ def upload():
     video_stream = video_file.read()
     with open(fname_video, 'wb') as f:
         f.write(video_stream)
-    
+
     one_week = 60 * 60 * 24 * 7
     fname, extension = fname_video.split('.')
     output_file = '{}-tracks{}.{}'.format(fname, time.time(), extension)
-   
+
     job = q.enqueue(
         'vision.get_tracking_video',
         args=(fname_video, output_file),
@@ -144,13 +144,12 @@ def upload():
     session['jobs'].append(job_dict)
     session.modified = True # In-place operations not picked up automatically
     logger.info(f'job: {job}')
-    
+
     return {
         'status': 200,
         'mimetype': 'application/json'
     }
 
-  
 @app.route('/test')
 def test():
     logger.info('test()')
